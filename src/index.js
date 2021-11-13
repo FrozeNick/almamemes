c957d913149eb4346bed6e1c0bb3e66e5b550713
FrozeNick <amirdelson@gmail.com>
2021-11-13 15:07:15 +0200

    Unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './lib/style/main.scss';
 import App from "./App";
+import './lib/style/main.scss';
 
 const rootElement = document.getElementById("root");
 render(
